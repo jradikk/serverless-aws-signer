3531fcfe5485e5d0dd2f48f9998a7a180761f4f7
Yevhenii Hordashnyk <yevhenii@alpacked.io>
2020-12-02 18:58:51 +0200

    Support signing functions individually

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const _ = require('lodash');
 
 const signersMethods = require('./src/SignersMethods');
 const cloudFormationGenerator = require('./src/cloudFormationGenerator');
@@ -33,71 +34,118 @@ class ServerlessPlugin {
     };
   }
 
-  signLambdas = async(serverless, options) => {
+  generateSignerConfiguration(lambda_function) {
 
-    this.serverless.cli.log('Signing functions');
-
-    var signerConfiguration = this.serverless.service.custom.signer;
-
-    
-    // Copy deployment artifact to S3
-    const fileContent = fs.readFileSync(this.serverless.service.package.artifact);
-
-    var S3Response = await this.serverless.providers.aws.request('S3', 'upload', {
-      Bucket: signerConfiguration.source.s3.bucketName, 
-      Key:  signerConfiguration.source.s3.key,
-      Body: fileContent
-    })
-
-    const S3ObjectVersion = S3Response.VersionId
-
-    var sign_params = {
+    const defaultConfig = {
       source: {
         s3: {
-          bucketName: signerConfiguration.source.s3.bucketName,
-          key: signerConfiguration.source.s3.key,
-          version: S3ObjectVersion
+          key: lambda_function.name+'-'+Math.floor(+new Date() / 1000),
         }
       },
       destination: {
         s3: {
-          bucketName: signerConfiguration.destination.s3.bucketName,
-          prefix: signerConfiguration.destination.s3.prefix,
+          prefix: "signed-"
         }
       },
-      profileName: signerConfiguration.profileName
+      profileName: this.serverless.service.service
     }
 
-    this.serverless.service.custom.signer;
+    const signerConfiguration = _.merge(defaultConfig, this.serverless.service.custom.signer, lambda_function.signer)
     
-    sign_params.source.s3.version = S3ObjectVersion;
-
-    // Start signing job
-    var signJob = await this.serverless.providers.aws.request('Signer', 'startSigningJob', sign_params)
-    
-    // Wait until Signing job successfully completes
-    var status = ""
-    while ( status !== "Succeeded" && status !== "Failed" ) {
-      var jobStatus = await this.serverless.providers.aws.request('Signer', 'describeSigningJob', {jobId: signJob.jobId})
-      status = jobStatus.status
+    try {
+      if (!signerConfiguration.source.s3.bucketName) {
+        throw new this.serverless.classes.Error("No bucket name was specified");
+      }
+        if (!signerConfiguration.destination.s3.bucketName) {
+          signerConfiguration.destination.s3.bucketName = signerConfiguration.source.s3.bucketName
+        }
+    }
+    catch {
+      throw new this.serverless.classes.Error("Incorrect signer plugin configuration");
     }
     
-    if (status === "Failed") {
-      throw new Error(`Signing job has failed with ${jobStatus.statusReason} reason`)
+    return signerConfiguration
+  }
+
+  signLambdas = async(serverless, options) => {
+
+    this.serverless.cli.log('Signing functions...');
+
+    var signerProcesses = {};
+
+    if (this.serverless.service.package.individually === true) {
+      const lambda_functions = this.serverless.service.functions;
+      for (let lambda_function in lambda_functions) {
+        signerProcesses[lambda_function] = {
+          signerConfiguration: this.generateSignerConfiguration(lambda_functions[lambda_function]),
+          packageArtifact: lambda_functions[lambda_function].package.artifact
+        }
+      }
+    }
+    else {
+      signerProcesses["common"] = {
+          signerConfiguration: this.serverless.service.custom.signer,
+          packageArtifact: this.serverless.service.package.artifact
+        }
     }
 
-    var signedCodeLocation = jobStatus.signedObject.s3
+    for (let lambda in signerProcesses) {
+      var signItem = signerProcesses[lambda];
+      // Copy deployment artifact to S3
+      const fileContent = fs.readFileSync(signItem.packageArtifact);
 
-    // Replace current zip archive of deployment archive with the same payload but signed
-      const { Body } = await this.serverless.providers.aws.request('S3', 'getObject', {
-        Bucket: signedCodeLocation.bucketName, 
-        Key:  signedCodeLocation.key
+      var S3Response = await this.serverless.providers.aws.request('S3', 'upload', {
+        Bucket: signItem.signerConfiguration.source.s3.bucketName, 
+        Key:  signItem.signerConfiguration.source.s3.key,
+        Body: fileContent
       })
-      await fs.writeFile(this.serverless.service.package.artifact, Body)
+
+      const S3ObjectVersion = S3Response.VersionId
+
+      var sign_params = {
+        source: {
+          s3: {
+            bucketName: signItem.signerConfiguration.source.s3.bucketName,
+            key: signItem.signerConfiguration.source.s3.key,
+            version: S3ObjectVersion
+          }
+        },
+        destination: {
+          s3: {
+            bucketName: signItem.signerConfiguration.destination.s3.bucketName,
+            prefix: signItem.signerConfiguration.destination.s3.prefix,
+          }
+        },
+        profileName: signItem.signerConfiguration.profileName
+      }
+
+      // Start signing job
+      var signJob = await this.serverless.providers.aws.request('Signer', 'startSigningJob', sign_params)
+
+      // Wait until Signing job successfully completes
+      var status = ""
+      while ( status !== "Succeeded" && status !== "Failed" ) {
+        var jobStatus = await this.serverless.providers.aws.request('Signer', 'describeSigningJob', {jobId: signJob.jobId})
+        status = jobStatus.status
+      }
+
+      if (status === "Failed") {
+        throw new Error(`Signing job has failed with ${jobStatus.statusReason} reason`)
+      }
+
+      var signedCodeLocation = jobStatus.signedObject.s3
+
+      // Replace current zip archive of deployment archive with the same payload but signed
+        const { Body } = await this.serverless.providers.aws.request('S3', 'getObject', {
+          Bucket: signedCodeLocation.bucketName, 
+          Key:  signedCodeLocation.key
+        })
+        await fs.writeFile(signItem.packageArtifact, Body)
+    }
   }
 
   addSigningConfigurationToCloudFormation = async(serverless, options) => {
-    this.serverless.cli.log('Updating signing configuration');
+    this.serverless.cli.log('Updating signing configuration...');
     var cloudFormationResources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
     var profileArn = await signersMethods.getProfileParamByName(this.serverless.service.custom.signer.profileName, 'profileVersionArn', this.serverless)
     
